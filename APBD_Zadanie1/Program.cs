2235ae9e6337ea43aa4f74c6003bde3b0c424185
s24171 <s24171@pjwstk.edu.pl>
2024-03-16 15:15:38 +0100

    Modyfikacja 3

--- APBD_Zadanie1/Program.cs
+++ APBD_Zadanie1/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("My name is Georgij.");
+Console.WriteLine("Enter name:");
+var name = Console.ReadLine();
+Console.WriteLine("The name is: " + name);
 Console.WriteLine("Enter your age:");
 var age = Console.ReadLine();
 Console.WriteLine("The age is: " + age);
+Console.WriteLine("Where are you studying?");
+var university = Console.ReadLine();
+Console.WriteLine(name + " has " + age + " years and studies at " + university + "university.");
