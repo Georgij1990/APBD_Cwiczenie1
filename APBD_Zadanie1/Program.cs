226a31362344f956f57845b3ef3ea6967b56aff4
s24171 <s24171@pjwstk.edu.pl>
2024-03-16 15:51:09 +0100

    Create static method to find max in the array

--- APBD_Zadanie1/Program.cs
+++ APBD_Zadanie1/Program.cs
@@ -15,3 +15,8 @@ static double findAverage(int[] ints) {
 int[] ints = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 var avg = findAverage(ints);
 Console.WriteLine(avg);
+
+static int findMax(int[] ints) {
+    return ints.Max();
+}
+Console.WriteLine(findMax(ints));
